289fd9d230153d9e0eeb81bc41191d27fdff1680
Saar <geger40.1@gmail.com>
2020-06-20 19:12:09 -0700

    Added completing items

--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -4,8 +4,9 @@
 				class="itemQty">{{item.qty}}x&nbsp;&nbsp;&nbsp;</span>
 			<template v-if="personal">
 				<label :for="item.id">Wishlist: </label>
-				<input type="checkbox" v-if="personal" @change="setWishlist" :id="item.id" :checked="item.onWishlist"/>
+				<input type="checkbox" v-if="personal" @change="setWishlist" :id="item.id" :checked="item.onWishlist"/>&nbsp;&nbsp;&nbsp;
 			</template>
+			<button @click="complete(item.id)">Complete</button>
 		</li>
 	</ul>
 </template>
@@ -27,6 +28,17 @@
 					credentials: "include",
 					body: json
 				});
+			},
+			complete(id) {
+				const data = {itemId: id};
+				const json = JSON.stringify(data);
+
+				fetch("http://localhost:80/completeItem",{
+					method: "POST",
+					credentials: "include",
+					body: json
+				})
+				.then(() => window.location.reload())
 			}
 		}
 	}
